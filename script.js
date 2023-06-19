bff5b2b32a9be53adf09401b9eba20e19f9af088
OGWhiskers <bayandadlamini02@gmail.com>
2023-06-19 19:50:07 +0200

    Add draft design for project section

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
 "use strict";
-
-const header = document.querySelector(".head-container");
-
-window.onscroll = () => sticky();
-
-let stickyVariable = header.offsetTop;
-
-let sticky = () => {
-  window.pageYOffset > stickyVariable
-    ? header.classList.add("sticky")
-    : header.classList.remove("sticky");
-};

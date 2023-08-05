351d214966327e99bc547eca105084c468b52e42
OGWhiskers <bayandadlamini02@gmail.com>
2023-08-05 11:40:16 +0200

    Add ecth-a-skecth project

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 "use strict";
+
+// ADD TARGET BLANK
+
+const aLinks = document.querySelectorAll(".btn");
+
+const addTarget = () =>
+  aLinks.forEach((item) => item.setAttribute("target", "_blank"));
+addTarget();
